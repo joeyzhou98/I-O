e414a32488f4409fc9ada7ffa9465d39d7cbc259
v2thegreat <aliabbaszaidi@outlook.com>
2018-04-10 16:00:23 -0400

    Renamed and added functionality
    Renamed from Assignment-5.cpp to main.cpp to make it clearer
    
    Added functionality to ensure checks that everything works fine. This might be a useful way to check issues

---
+++ main.cpp
@@ -0,0 +1,18 @@
+﻿
+#include "stdafx.h"
+#include "Ingredients.h"
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+	const char * fname = "sample.json";
+	vector<Items> AllItems= getItems(fname); //Getting all the items from the Json file
+	cout << "ID: " << AllItems[0].id << endl;
+	cout << "If no errors raised, it works!";
+	cin.ignore();
+	return 0;
+}
